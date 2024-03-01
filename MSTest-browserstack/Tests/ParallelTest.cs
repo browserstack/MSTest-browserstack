b1dc9ae4721bb578893acde70dbeb668eeea4501
Aaryan <aaryan@browserstack.com>
2024-03-01 14:00:26 +0530

    added selenium-3 mstest sample

---
+++ MSTest-browserstack/Tests/ParallelTest.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using OpenQA.Selenium;
+
+namespace MSTest_browserstack;
+
+[TestClass]
+public class ParallelTest
+{
+    [TestMethod]
+    public void SearchBstackDemo()
+    {
+        ParallelTests.RunParallelTests();
+    }
+}
+
+public class ParallelTests
+{
+    public static void RunParallelTests()
+    {
+        System.Collections.Generic.Dictionary<string, object> browserstackOptions =
+            new Dictionary<string, object>
+            {
+                { "browser", "iPhone" },
+                { "os_version", "14" },
+                { "device", "iPhone 12" },
+                { "realMobile", "true" },
+                { "browserstack.local", "false" },
+                { "build", "browserstack-build-1" },
+                { "name", "BStack test" },
+                { "browserstack.source", "mstest:sample" }
+            };
+        Thread device1 = new Thread(() => RunTest("safari", browserstackOptions));
+        Thread device2 = new Thread(() => RunTest("safari", browserstackOptions));
+        Thread device3 = new Thread(() => RunTest("safari", browserstackOptions));
+        Thread device4 = new Thread(() => RunTest("safari", browserstackOptions));
+
+        device1.Start();
+        device2.Start();
+        device3.Start();
+        device4.Start();
+
+        device1.Join();
+        device2.Join();
+        device3.Join();
+        device4.Join();
+    }
+
+    private static void RunTest(string browser, Dictionary<string, object> browserstackOptions)
+    {
+        IWebDriver? driver = BrowserStackConfiguration.ConfigureBrowserStack(browser, browserstackOptions);
+
+        BrowserStackConfiguration.RunTest(driver);
+    }
+}
---
+++ MSTest-browserstack/Tests/SingleTest.cs
@@ -0,0 +1,34 @@
+﻿using OpenQA.Selenium;
+using System.Collections.Generic;
+
+namespace MSTest_browserstack;
+
+[TestClass]
+public class SingleTest : BrowserStackConfiguration
+{
+    protected static IWebDriver? driver;
+
+    [TestInitialize]
+    public void TestInitialize()
+    {
+        System.Collections.Generic.Dictionary<string, object> browserstackOptions =
+            new Dictionary<string, object>
+            {
+                { "browser", "iPhone" },
+                { "os_version", "14" },
+                { "device", "iPhone 12" },
+                { "realMobile", "true" },
+                { "browserstack.local", "false" },
+                { "build", "browserstack-build-1" },
+                { "name", "BStack test" },
+                { "browserstack.source", "mstest:sample" }
+            };
+        driver = ConfigureBrowserStack("safari", browserstackOptions);
+    }
+
+    [TestMethod]
+    public void SearchBstackDemo()
+    {
+        RunTest(driver);
+    }
+}
